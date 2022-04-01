1648025413b79e148da9c3b4edef9902769f9721
TKDuff <94256721+TKDuff@users.noreply.github.com>
2022-04-01 22:04:22 +0100

    More efficient

--- Lab08Bitwise.java
+++ Lab08Bitwise.java
@@ -1,78 +1,72 @@
 import java.util.Scanner;
 public class Lab08Bitwise {
+	/*
+	First line contains an integer which is the base the number N is represented in (base 1)
+	Second line contains an integer which is the base that the number N should be transformed into (base 2)
+	Third line contains an integer N
 	
-	String val = "";
-	
+	*/
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int b1 = in.nextInt();
-        int b2 = in.nextInt();
-        int n = in.nextInt();
-		int store = 0;
-				
+		int b2 = in.nextInt();
+		String n = in.next();
+		
+		
 		if(b1 == 16) {
-			out(hexTo(String.valueOf(n)), b2);
-		} else if (b2 == 16) {
-			//int N = Integer.parseInt(n);
-			int power = 0;
-			while(n != 0) {
-				store += n%10 * Math.pow(b1, power);
-				n /= 10;
-				power++;
-			}
-			System.out.println(hex(store, b2));
+			System.out.println(hexToDec(n, b2));
+		}else if(b2 == 16) {
+			int dec = toDecimal(Integer.parseInt(n), b1);
+			System.out.println(DecToHex(dec));
 		} else {
-			//int N = Integer.parseInt(n);
-		int power = 0;
+			int dec = toDecimal(Integer.parseInt(n), b1);
+			System.out.println(decimalToBase(dec, b2));
+		}
+	}
+	
+	static int toDecimal(int n, int b1) {
+		int store = 0, power = 0;
 		while(n != 0) {
 			store += n%10 * Math.pow(b1, power);
 			n /= 10;
 			power++;
-		}		
-		System.out.println(out(store, b2));
-		} 
-	}
-	
-	static int hexTo (String param) {
-		int store = 0;
-		int pow = 0;
-		String values = "ABCDEF";
-		for(int i = param.length()-1; i >=0; i--) {
-			char current = param.charAt(i);
-			if(!Character.isDigit(current)) {
-				store += (values.indexOf(current) + 10) * (Math.pow(16, pow));
-			} else {
-				store += Character.getNumericValue(current) * (Math.pow(16, pow));
-			}
-			pow++;
 		}
 		return store;
 	}
+		
 	
-	static String out (int param, int b2) {
+	static String decimalToBase (int param, int b2) {
 		if(param == 0) {
 			return "";
 		} else {
-			return out( (param - (param%b2))/b2, b2) + String.valueOf(param%b2);
+			return decimalToBase( (param - (param%b2))/b2, b2) + String.valueOf(param%b2);
 		}	
 	}
 	
-	static String hex (int param, int b2) {
+	static int hexToDec (String param, int b2) {
+		int pow = 0;
+		int store = 0;
+		String values = "123456789ABCDEF";
+		
+		
+		while(pow < param.length()) {
+			int index = param.length()-1 - pow;			//counting from right of string, starting at 0, as move left increment by one
+			store += (values.indexOf(param.charAt(index))+1) * Math.pow(16, pow);
+			pow++;
+		}
+		//return store;
+		if(b2 == 10)			//if base 16 need to be changed to base 10 no point in converting base 10 output to base 10 again
+			return store;
+		else
+			return Integer.parseInt(decimalToBase(store, b2));
+	}
+	
+	static String DecToHex (int param) {
 		String[] val = {"1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
 		if(param == 0) {
 			return "";
 		} else {
-			return out( (param - (param%b2))/b2, b2) + val[(param%b2)-1];
-		}	
-	}
-	
-	static int bin( String param) {
-		int pow = 0;
-		int st = 0;
-		for(int i = 0; i <param.length(); i++ ) {
-			int current = Character.getNumericValue(param.charAt(i));
-			st += current * Math.pow(2, (i+1));
+			return DecToHex( (param - (param%16))/16) + val[(param%16)-1];
 		}
-		return st;
-	}
+}
 }
